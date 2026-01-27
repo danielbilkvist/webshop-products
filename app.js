ac6d8d5476ee72180f760f4c665c5a412abb8e93
danielbilkvist <danielbilkvist@gmail.com>
2026-01-27 11:01:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,3 +8,17 @@ document.addEventListener("DOMContentLoaded", initApp);
 function initApp() {
   console.log("App initialized");
 }
+
+const productTitle = "Fjallraven - Foldsack No. 1 Backpack";
+const productPrice = 849;
+const inStock = true;
+
+console.log(productTitle);
+console.log(typeof productTitle);
+console.log(productPrice);
+console.log(typeof productPrice);
+console.log(inStock);
+console.log(typeof inStock);
+
+
+const testSection = document.querySelector)
