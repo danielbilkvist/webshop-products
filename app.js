621c704dd376d84d0059da354fe7e89b99a527b8
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2026-01-27 09:39:34 +0100

    Remove initial HTML template from app.js

--- app.js
+++ app.js
@@ -8,11 +8,3 @@ document.addEventListener("DOMContentLoaded", initApp);
 function initApp() {
   console.log("App initialized");
 }
-
-const html = /*html*/ `
-  <h2>Welcome to the App</h2>
-  <img src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png" class="product-image" />
-  <p>This is a simple application initialized on DOMContentLoaded event.</p>
-  <section></section>
-  <p></p>
-`;
